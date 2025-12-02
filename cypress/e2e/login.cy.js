72b9d782e8b444188e846ffdbe1d78cca15f0283
vouandzanoeldy@gmail.com <vouandzanoeldy@gmail.com>
2025-12-02 15:40:22 +0100

    mod

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,6 +1,6 @@
 /// <reference types="cypress" />
 
-  it(' login valid',{tag: "@valid"}, () => { 
+  it(' login avec des credentiels valide',{tags:[ "@valid","@cedric"]}, () => { 
     cy.visit("https://www.saucedemo.com")
     cy.get("#user-name").type("standard_user1")
     cy.get("#password").type("secret_sauce")

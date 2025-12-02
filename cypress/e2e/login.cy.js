ba1a38b148f636e144f7c9b5a828b6c70f6d7a45
abdouDev194 <abdenournour782@gmail.com>
2025-12-02 15:16:39 +0100

    user

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,17 @@
+/// <reference types="cypress" />
+
+  it(' login valid',{tag: "@valid"}, () => { 
+    cy.visit("https://www.saucedemo.com")
+    cy.get("#user-name").type("standard_user1")
+    cy.get("#password").type("secret_sauce")
+    cy.get("#login-button").click()
+    cy.get("span.title").should("be.visible")
+  })
+
+   it(' login invalid',  {tag: "@invalid"},() => { 
+    cy.visit("https://www.saucedemo.com")
+    cy.get("#user-name").type("standard_user1")
+    cy.get("#password").type("Abdou")
+    cy.get("#login-button").click()
+    cy.get("h3[data-test='error']").should("be.visible")
+   })
